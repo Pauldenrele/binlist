9f03b9138582e100c0644963cf9cbf1fbead9061
PAULI <adenreleademola@yahoo.com>
2019-12-29 19:11:13 +0100

    First commit

---
+++ app/src/main/java/com/example/binlist/EditAcitivity.kt
@@ -0,0 +1,12 @@
+package com.example.binlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAcitivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_acitivity)
+    }
+}
